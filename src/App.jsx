c5a1ff0829495b238f8ad4e9480e40f232dcadca
Eriquy Jhordan <eriquy@gmail.com>
2023-10-19 15:27:05 -0300

    redirect the page

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { models } from 'powerbi-client';
 
 function App() {
   setTimeout(() => {
-    window.location.reload();
+    window.location.href = "https://apps.powerapps.com/play/e/default-203bc81b-c25f-4cd3-a09b-6476c30bb289/a/083fbc08-0af7-4aaa-b917-e7bbbb66571d?tenantId=203bc81b-c25f-4cd3-a09b-6476c30bb289&hint=077b610a-1471-4cde-8441-22c947fbec81&sourcetime=1697736325808";
   }, 5 * 60 * 1000);
   return (
     <div className="h-screen w-screen">
       <PowerBIEmbed
         embedConfig={{
           type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-          id: 'a4fc3710-21a0-48c0-a0ac-844b617e3275',
-          embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=a4fc3710-21a0-48c0-a0ac-844b617e3275&groupId=124f88d3-8d5b-4438-996c-1fb90c106f35&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUJSQVpJTC1TT1VUSC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d',
+          id: '0451e003-388b-41b8-a83e-d8d6a8f32d62',
+          embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=0451e003-388b-41b8-a83e-d8d6a8f32d62&groupId=c6e10a79-f6ed-4665-a30b-31bfcd6c9029&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLUJSQVpJTC1TT1VUSC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d',
           accessToken: process.env.REACT_APP_EMBED_TOKEN,
           tokenType: models.TokenType.Aad,
           settings: {
